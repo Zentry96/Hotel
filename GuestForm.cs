721e13ddd1f4954ea8d6bac7e425b00334ea7566
Lazar Milanovic <llmilanovic96@gmail.com>
2022-06-10 15:00:39 +0200

    dodat try/catch kod dugmeta za cuvanje novog gosta kao i if funkcija koja proverava da li su sva polja popunjena

--- GuestForm.cs
+++ GuestForm.cs
@@ -29,20 +29,34 @@ namespace Hotel
 
         private void button_save_Click(object sender, EventArgs e)
         {
-            string id = textBox_id.Text;
-            string fname = textBox_fName.Text;
-            string lname = textBox_lName.Text;
-            string phone = textBox_phone.Text;
-            string city = textBox_city.Text;
-
-            Boolean insertGuest = guest.insertGuest(id, fname, lname, phone, city);
-            if (insertGuest)
+            if (textBox_id.Text == "" || textBox_fName.Text == "" || textBox_phone.Text == "" || textBox_lName.Text == "" || textBox_city.Text == "")
             {
-                MessageBox.Show("Novi gost uspešno dodat", "Gost sačuvan", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                MessageBox.Show("Niste uneli sve podatke", "Nedostatak podataka", MessageBoxButtons.OK, MessageBoxIcon.Information);
             }
             else
             {
-                MessageBox.Show("Dodavanje novog gosta nije uspelo", "Greška pri dodavanju", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                try
+                {
+                    string id = textBox_id.Text;
+                    string fname = textBox_fName.Text;
+                    string lname = textBox_lName.Text;
+                    string phone = textBox_phone.Text;
+                    string city = textBox_city.Text;
+
+                    Boolean insertGuest = guest.insertGuest(id, fname, lname, phone, city);
+                    if (insertGuest)
+                    {
+                        MessageBox.Show("Novi gost uspešno dodat", "Gost sačuvan", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                    else
+                    {
+                        MessageBox.Show("Dodavanje novog gosta nije uspelo", "Greška pri dodavanju", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
             }
         }
     }
